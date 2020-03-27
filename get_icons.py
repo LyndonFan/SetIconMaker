ee85b9e05b3390c1ece474d923d3cad7f72801c8
LyndonFan <lyndon0808@gmail.com>
2020-03-27 15:55:31 +0800

    add useful snippets

--- get_icons.py
+++ get_icons.py
@@ -11,4 +11,9 @@ def get_icons(image,contours,obb):
         M = cv2.getRotationMatrix2D(center=image.shape //2, angle = obb.angle)
 
 if __name__ == "__main__":
-    #(thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
+
+'''
+gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY) # convert to grayscale
+blur = cv2.blur(gray, (3, 3)) # blur the image
+ret, thresh = cv2.threshold(blur, 50, 255, cv2.THRESH_BINARY)
+'''
