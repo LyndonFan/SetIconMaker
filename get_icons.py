bffc892c97ac338ce83790eca0708126917cf913
LyndonFan <lyndon0808@gmail.com>
2020-03-28 10:57:52 +0800

    can get image

--- get_icons.py
+++ get_icons.py
@@ -5,13 +5,50 @@ from get_contours import *
 from get_box import *
 
 def get_icons(image,contours,obb):
+    
+    box = cv2.boxPoints(obb)
+    box = np.int0(box)
+
     for x in "urm":
+        im = image
         background = cv2.imread(str(x)+".png")
-        new_size = obb.shape
-        M = cv2.getRotationMatrix2D(center=image.shape //2, angle = obb.angle)
+        M_rot = cv2.getRotationMatrix2D(center=(background.shape[0]/2,background.shape[1]/2), angle = obb[-1]/np.pi*180,scale=1)
+        rot_background = cv2.warpAffine(background,M_rot,background.shape[:2])
+        mid_pt_x = (obb[0][0]+obb[1][0])/2
+        mid_pt_y = (obb[0][0]+obb[1][0])/2
+        M_trans = np.float32([[1,0,mid_pt_x-background.shape[0]/2],[0,1,mid_pt_y-background.shape[1]/2]])
+        trans_background = cv2.warpAffine(rot_background,M_trans,(image.shape[1]+PAD_WIDTH*2,image.shape[0]+PAD_WIDTH*2))
+        cv2.imshow(x,trans_background)
+        cv2.waitKey(0)
+
+        im = im.reshape(image.shape[0],image.shape[1],1)
+        print(im.shape)
+        im = np.repeat(im,repeats=3,axis=2)
+        padded = np.pad(im, ((PAD_WIDTH,PAD_WIDTH),(PAD_WIDTH,PAD_WIDTH),(0,0)), constant_values=255)
+        print(padded.shape)
+        print(trans_background.shape)
+        padded = cv2.bitwise_not(padded)
+        res = cv2.bitwise_and(padded,trans_background)
+        padded = cv2.bitwise_not(padded)
+        cv2.imshow(x,res)
+        res = cv2.bitwise_or(padded,trans_background)
+        cv2.drawContours(res,contours,-1,(0,0,0),10,offset=(PAD_WIDTH,PAD_WIDTH))
+        cv2.imshow(x,res)
+        cv2.waitKey(0)
+        
 
 if __name__ == "__main__":
+    image = cv2.imread('test2.png') 
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    contours = get_contours(image)
+    obb = get_box_alt(image,contours)
+    get_icons(gray,contours,obb)
 
+    image = cv2.imread('test.png') 
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    contours = get_contours(image)
+    obb = get_box_alt(image,contours)
+    get_icons(gray,contours,obb)
 '''
 gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY) # convert to grayscale
 blur = cv2.blur(gray, (3, 3)) # blur the image
