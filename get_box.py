d91b813c4d9162bd6cced93bc73cd049b363235a
LyndonFan <lyndon0808@gmail.com>
2020-03-28 11:27:43 +0800

    remove old fn

--- get_box.py
+++ get_box.py
@@ -7,28 +7,6 @@ PAD_WIDTH = 20
 
 def get_box(im,contours):
 
-    print(len(contours))
-    gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-    im_padded = np.pad(im, ((PAD_WIDTH,PAD_WIDTH),(PAD_WIDTH,PAD_WIDTH),(0,0)), constant_values=255)
-    rects = []
-    for cnt in contours:
-        rect = cv2.minAreaRect(cnt)
-        box = cv2.boxPoints(rect)
-        box = np.int0(box)
-        if cv2.contourArea(box) >= 25:
-            print(box)
-            rects.append(rect)
-            cv2.drawContours(im_padded,[box],0,(0,255,0),10,offset=(PAD_WIDTH,PAD_WIDTH))
-    
-    print(rects)
-
-    cv2.imshow("w/ box",gray)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    return rect
-
-def get_box_alt(im,contours):
-
     im_padded = im
     print(im_padded.shape)
     im_padded = np.pad(im, ((PAD_WIDTH,PAD_WIDTH),(PAD_WIDTH,PAD_WIDTH),(0,0)), constant_values=255)
@@ -57,12 +35,10 @@ if __name__ == "__main__":
     gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
     contours = get_contours(image)
     get_box(image,contours)
-    get_box_alt(image,contours)
 
     image_2 = cv2.imread('test2.png')
     gray_2 = cv2.cvtColor(image_2,cv2.COLOR_BGR2GRAY)
     contours_2 = get_contours(image_2)
-    box_2 = get_box(image_2,contours_2)
-    box_2 = get_box_alt(image_2,contours_2)
+    get_box(image_2,contours_2)
 
 
